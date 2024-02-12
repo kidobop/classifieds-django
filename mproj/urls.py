766837a49840507a8641d24244bf7676e8052ef8
aaqil <kidobop@gmail.com>
2024-02-12 23:14:55 +0530

    added a page for item details

--- mproj/urls.py
+++ mproj/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from core.views import index
 from core.views import contact
+from django.urls import path, include
 
 
 urlpatterns = [
     path('',index, name='index'),
+    path('items/',include('item.urls')),
     path("contact/",contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
