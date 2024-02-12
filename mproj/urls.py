aab316a2f829fa1d79eeefde3dc7db7b13e5176e
aaqil <kidobop@gmail.com>
2024-02-13 00:20:45 +0530

    added a signup login

--- mproj/urls.py
+++ mproj/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('',include('core.urls')),
     path('items/',include('item.urls')),
-    path("contact/",contact, name="contact"),
+    # path("contact/",contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
